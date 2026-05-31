104583a58302e22ae3873d21bd2fe4cccfa271d9
Admin <mail@example.com>
2026-05-31 18:13:45 +0300

    feat: add RootLayout with Google Analytics and Vercel Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from '@vercel/analytics/react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
+        <GoogleAnalytics gaId="G-W8FP7FW13K" />
       </body>
     </html>
   );
